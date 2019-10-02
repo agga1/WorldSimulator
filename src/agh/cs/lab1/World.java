a1eb90d55dd99c3aafcf2e600eacbfb27cf9d2fa
agga1 <agud100@gmail.com>
2019-10-02 22:16:13 +0200

    stream version

--- src/agh/cs/lab1/World.java
+++ src/agh/cs/lab1/World.java
@@ -1,82 +1,68 @@
 package agh.cs.lab1;
+
 import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.lang.System.out;
+
 public class World {
     public static void main(String[] args) {
         System.out.println("Start systemu");
-        Direction[] dirs =toEnum(args);
+        Direction[] dirs = toEnum(args);
         run(dirs);
         System.out.println("Zakończenie systemu");
     }
-    static Direction[] toEnum(String[] ar){
-        Direction[] ans = new Direction[ar.length];
-        int i=0;
-        for(String str : ar){
-            switch(str){
-                case "f":
-                    ans[i++]= Direction.FORWARD;
-                    break;
-                case "b":
-                    ans[i++]= Direction.BACKWARD;
-                    break;
-                case "r":
-                    ans[i++]= Direction.RIGHT;
-                    break;
-                case "l":
-                    ans[i++]= Direction.LEFT;
-                    break;
-            }
-        }
 
-        return  Arrays.copyOfRange(ans, 0, i );
+    private static Direction[] toEnum(String[] args) {
+        String[] res1 = Arrays.stream(args).filter(World::filterInapt).toArray(String[]::new);
+        Direction[] res2 = Arrays.stream(res1).map(World::mapToEnum).toArray(Direction[]::new);
+        out.println(res1.length);
+        return res2;
     }
-    static void run(Direction[] dirs){
+
+    private static Boolean filterInapt(String arg) {
+        return (arg.equals("f") || arg.equals("b") || arg.equals("r") || arg.equals("l"));
+    }
+
+    private static Direction mapToEnum(String arg) {
+        Direction ans = null;
+        switch (arg) {
+            case "f":
+                ans = Direction.FORWARD;
+                break;
+            case "b":
+                ans = Direction.BACKWARD;
+                break;
+            case "r":
+                ans = Direction.RIGHT;
+                break;
+            case "l":
+                ans = Direction.LEFT;
+                break;
+        }
+        return ans;
+    }
+    private static void printText(Direction d){
+        switch (d) {
+            case FORWARD:
+                out.println("zwierzak idzie do przodu");
+                break;
+            case BACKWARD:
+                out.println("zwierzak idzie do tylu");
+                break;
+            case RIGHT:
+                out.println("zwierzak skręca w prawo");
+                break;
+            case LEFT:
+                out.println("zwierzak skręca w lewo");
+                break;
+        }
+    }
+    private static void run(Direction[] dirs) {
+        Stream<Direction> res1 = Arrays.stream(dirs);
         out.println("start");
-        for(Direction dir : dirs){
-            switch(dir){
-                case FORWARD:
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case RIGHT:
-                    out.println("zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    out.println("zwierzak skręca w lewo");
-                    break;
-            }
-        }
-        /* for run(String[] ar)
-        out.println("start");
-        for(String str : ar){
-            switch(str){
-                case "f":
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case "b":
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case "r":
-                    out.println("zwierzak skręca w prawo");
-                    break;
-                case "l":
-                    out.println("zwierzak skręca w lewo");
-                    break;
-            }
-        }
-        out.println("Stop");
-        */
-        /*
-        System.out.println("zwierzak idzie do przodu");
-        String ans="";
-        for(String animal : ar){
-            ans += animal + ", ";
-        }
-        ans = ans.substring(0, ans.length() -2);
-        System.out.println(ans);
-        */
+        res1.forEach(World::printText);
+        out.println("end");
     }
 }
